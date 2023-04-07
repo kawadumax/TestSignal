ce9ffa77d5d8600942201fd64b78928507128032
おるとろ <kazmax7@gmail.com>
2023-04-07 17:41:49 +0900

    Adjust GUI Position

--- Source/PluginEditor.cpp
+++ Source/PluginEditor.cpp
@@ -26,10 +26,11 @@ TestSignalAudioProcessorEditor::TestSignalAudioProcessorEditor(TestSignalAudioPr
 	// editor's size to whatever you need it to be.
 	DBG("Editor Constructed");
 	constexpr int width = 600;
-	constexpr int height = 300;
+	constexpr int height = 130;
 	constexpr float centerX = width / 2;
 	constexpr float centerY = height / 2;
 	constexpr float fontSize = 14.0f;
+	constexpr float margin = 20;
 
 	// Make sure that before the constructor has finished, you've set the
 	// editor's size to whatever you need it to be.
@@ -37,7 +38,17 @@ TestSignalAudioProcessorEditor::TestSignalAudioProcessorEditor(TestSignalAudioPr
 
 	// Create a ComboBox
 	noiseTypeComboBox.addItemList(Constants::NOISE_OPTIONS, 1);
-	noiseTypeComboBox.setBounds(10, 50, 100, 30);
+	noiseTypeComboBox.setJustificationType(juce::Justification::centred);
+	noiseTypeComboBox.setBounds(
+		0,
+		0,
+		width / 4,
+		fontSize + margin
+	);
+	noiseTypeComboBox.setCentrePosition(
+		centerX,
+		margin + (fontSize + margin) / 2
+	);
 	noiseTypeAttachment.reset(
 		new ComboBoxAttachment(
 			apvts,
@@ -51,20 +62,29 @@ TestSignalAudioProcessorEditor::TestSignalAudioProcessorEditor(TestSignalAudioPr
 
 	auto slider = std::make_unique<juce::Slider>();
 	slider->setRange(0.0, 1.0, 0.01);
-	slider->setSliderStyle(juce::Slider::SliderStyle::LinearBar);
-	slider->setTextBoxStyle(juce::Slider::TextEntryBoxPosition::TextBoxAbove, true, 200, fontSize + 5);
-	slider->setBounds(200, 200, 400, 400);
+	slider->setSliderStyle(juce::Slider::SliderStyle::LinearHorizontal);
+	slider->setTextBoxStyle(juce::Slider::TextEntryBoxPosition::TextBoxRight, true, fontSize * 4 + margin, fontSize + margin);
+	slider->setBounds(
+		0,
+		0,
+		width / 2,
+		fontSize + margin
+	);
+	slider->setCentrePosition(
+		centerX + 30,
+		noiseTypeComboBox.getBottom() + margin + (fontSize + margin) / 2
+	);
 
 	//Make label for knobs
 	auto label = std::make_unique<juce::Label>();
 	label->setFont(juce::Font(fontSize));
 	label->setText(Constants::PARAMETER_NAME::GAIN_NAME, juce::dontSendNotification);
-	label->setJustificationType(juce::Justification::centred);
+	label->setJustificationType(juce::Justification::centredLeft);
 	label->setBounds(
-		slider->getX(),
-		slider->getBottom() - 5,
-		slider->getWidth(),
-		fontSize
+		centerX - slider->getWidth() / 2 - fontSize * 4 + margin + 30,
+		noiseTypeComboBox.getBottom() + margin,
+		fontSize * 4 + margin,
+		fontSize + margin
 	);
 
 	addAndMakeVisible(slider.get());
@@ -88,9 +108,9 @@ void TestSignalAudioProcessorEditor::paint(juce::Graphics& g)
 	// (Our component is opaque, so we must completely fill the background with a solid colour)
 	g.fillAll(getLookAndFeel().findColour(juce::ResizableWindow::backgroundColourId));
 
-	g.setColour(juce::Colours::white);
-	g.setFont(15.0f);
-	g.drawFittedText("Hello World!", getLocalBounds(), juce::Justification::centred, 1);
+	//g.setColour(juce::Colours::white);
+	//g.setFont(15.0f);
+	//g.drawFittedText("Hello World!", getLocalBounds(), juce::Justification::centred, 1);
 }
 
 void TestSignalAudioProcessorEditor::resized()
